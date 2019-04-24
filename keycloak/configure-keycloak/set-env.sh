62e0f5cde4654eb920ab18fafe9794274f2700bb
sebastienblanc <scm.blanc@gmail.com>
2019-04-24 16:30:23 +0200

    update to keycloak 6.0.0

--- keycloak/configure-keycloak/set-env.sh
+++ keycloak/configure-keycloak/set-env.sh
@@ -1,7 +1,7 @@
-curl https://downloads.jboss.org/keycloak/5.0.0/keycloak-5.0.0.zip --output keycloak-5.0.0.zip
+curl https://downloads.jboss.org/keycloak/6.0.0/keycloak-6.0.0.zip --output keycloak-6.0.0.zip
 yum install -y unzip
-unzip keycloak-5.0.0.zip
-cd keycloak-5.0.0/bin
+unzip keycloak-6.0.0.zip
+cd keycloak-6.0.0/bin
 ./add-user-keycloak.sh -r master -u admin -p admin
 ./standalone.sh -b 0.0.0.0
 
